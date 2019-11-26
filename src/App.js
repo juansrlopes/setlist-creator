c126a594fd450133bda0ab4881d81a63534b0249
Juan <juan@realworks.nl>
2019-11-26 16:34:43 +0100

    Starting app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+    const [bandData, setBandData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios("http://localhost:3000/bands.json");
+            setBandData(result.data.bands);
+        };
+        fetchData();
+    }, []);
 
-export default App;
+    console.log("bandData : ", bandData);
+    return <div className="App">Hello</div>;
+};
